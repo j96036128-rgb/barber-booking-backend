7c878c2b708a0eff21ca4f08361058ece6c93782
Jon McMahon <jam@Jons-MacBook-Pro.local>
2026-01-01 14:44:18 +0000

    Remove prisma.config.ts and fix Prisma for Render

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "npx ts-node --project tsconfig.json prisma/seed.ts",
-  },
-});
